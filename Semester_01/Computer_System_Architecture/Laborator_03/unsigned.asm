b60172a584d42034375e43206cb0c5b1495912fd
Cristian Crețu <crisemcr@gmail.com>
2023-10-18 18:57:20 +0300

    feat: add unsigned

--- Semester_01/Computer_System_Architecture/Laborator_03/unsigned.asm
+++ Semester_01/Computer_System_Architecture/Laborator_03/unsigned.asm
@@ -11,13 +11,14 @@ segment data use32 class=data
     c dw 2
     e dd 4
     x dq 8
+; for this input, the result should be
+; (1 + 2 * 2 + 2 / 2) / (2 + 1) + 4 + 8 = 0xE = 14
 
 segment code use32 class=code
     start:
         ; we clear the registers
         mov EAX, 0
         mov EBX, 0
-        mov ECX, 0
         mov EDX, 0
         
         ; Calculate b * c
@@ -27,7 +28,7 @@ segment code use32 class=code
         mov CX, AX ; store the result in CX
         
         ; Calculate 2 / c
-        mov AL, 2        
+        mov AX, 2   
         div word [c]
         
         ; Add a + b * c + 2 / c
@@ -35,15 +36,12 @@ segment code use32 class=code
         adc AH, 0 ; if it overflows, add the carry
         add AX, CX ; CX stores the b * c, so we add it
         adc EDX, 0
-        mov ECX, EDX ; store the high 16 bits
         
         ; Calculate 2 + a
         mov BL, byte [a]
         add BL, 2
-        div BL ; divide by (2 + a)
-        
-        add AH, CL ; add high 16 bits to counter
-        
+        div BL ; divide (a+b*c+2/c) by (2 + a)
+                
         ; Add e to that result
         add EAX, [e]
         adc EDX, 0 ; if it's bigger than 32 bits, we put the carry into EDX
