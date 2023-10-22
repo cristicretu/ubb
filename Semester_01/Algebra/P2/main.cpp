beff39d9074969a97031d26de4682d62f74da45a
Cristian Crețu <crisemcr@gmail.com>
2023-10-22 12:40:16 +0300

    feat: finish

--- Semester_01/Algebra/P2/main.cpp
+++ Semester_01/Algebra/P2/main.cpp
@@ -1,11 +1,31 @@
+/*
+P2. Count the number of associative operations on a set A of size n. For n <= 4,
+print the operation tables of each associative operation on a set A of size n.
+
+@Author: Cretu Cristian, 913
+
+Usage: ./main 01_input.txt
+*/
+
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <sstream>
+#include <thread>
 #include <vector>
 
-int n;
-std::vector<std::vector<std::vector<int>>> tables;
+// Global variables
+int n;                                             // Size of the set A
+std::vector<std::vector<std::vector<int>>> tables; // Vector of all the
+                                                   // associative tables
+std::mutex mtx; // Mutex to ensure thread safety when modifying the global
+// tables vector
 
+/*
+ Check if a binary operation for a set A is associative
+ Associativity is defined by the following property:
+    (a * b) * c = a * (b * c)
+ */
 bool isAssociative(const std::vector<std::vector<int>> &table) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
@@ -18,22 +38,36 @@ bool isAssociative(const std::vector<std::vector<int>> &table) {
   return true;
 }
 
+// Recursive backtracking function to generate all possible operation tables and
+// check if they are associative
 void generateTables(int i, int j, std::vector<std::vector<int>> &table) {
   if (i == n) {
-    if (isAssociative(table))
+    if (isAssociative(table)) {
+      mtx.lock(); // Lock to ensure thread safety
       tables.push_back(table);
+      mtx.unlock(); // Release the lock
+    }
     return;
   }
   for (int x = 0; x < n; x++) {
     table[i][j] = x;
-    if (j == n - 1)
+    if (j == n - 1) // If end of row, move to next row
       generateTables(i + 1, 0, table);
     else
       generateTables(i, j + 1, table);
   }
 }
 
+// Threaded function to distribute the task of generating tables
+// Speeds up the execution time by a factor of ~4x on my machine
+void threadedGenerateTables(int startValue) {
+  std::vector<std::vector<int>> table(n, std::vector<int>(n, 0));
+  table[0][0] = startValue;
+  generateTables(0, 1, table);
+}
+
 int main(int argc, char *argv[]) {
+  // Check for correct number of command line arguments
   if (argc != 2) {
     std::cout << "Usage: " << argv[0] << " input_file.txt" << std::endl;
     return 1;
@@ -47,7 +81,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  inFile >> n;
+  inFile >> n; // Read the size of the set A
   if (inFile.fail()) {
     std::cout << "Invalid input in '" << input_file
               << "'. Please provide a single integer on the first line."
@@ -55,12 +89,24 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  // Derive output file name from the input file name
   std::string baseName = input_file.substr(0, input_file.find('_'));
   std::string output_file = baseName + "_output.txt";
   std::ofstream outFile(output_file);
 
-  std::vector<std::vector<int>> table(n, std::vector<int>(n, 0));
-  generateTables(0, 0, table);
+  int numThreads =
+      std::thread::hardware_concurrency(); // Get number of supported threads
+  std::vector<std::thread> threads;
+
+  // Launch threads for each possible start value
+  for (int i = 0; i < numThreads && i < n; ++i) {
+    threads.push_back(std::thread(threadedGenerateTables, i));
+  }
+
+  // Wait for all threads to finish
+  for (auto &th : threads) {
+    th.join();
+  }
 
   outFile << "1. the number of associative operations on a set A of size " << n
           << " is: " << tables.size() << std::endl;
