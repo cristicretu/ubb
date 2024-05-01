ea9d29dd5e4449c21a24c76c868b570442c3551b
Cristian Cretu <crisemcr@gmail.com>
2024-05-01 20:53:51 +0300

    a

---
+++ Semester_02/DSA/L4_SET/App.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+
+#include "ExtendedTest.h"
+#include "Set.h"
+#include "SetIterator.h"
+#include "ShortTest.h"
+using namespace std;
+
+int main() {
+  testAll();
+  testAllExtended();
+
+  cout << "That's all!" << endl;
+}
