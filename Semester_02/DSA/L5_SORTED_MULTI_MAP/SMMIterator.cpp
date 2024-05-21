ffbc7b25b32fc6a8c748666e43f990daf527cc34
Cristian Cretu <crisemcr@gmail.com>
2024-05-21 21:20:55 +0300

    a

--- Semester_02/DSA/L5_SORTED_MULTI_MAP/SMMIterator.cpp
+++ Semester_02/DSA/L5_SORTED_MULTI_MAP/SMMIterator.cpp
@@ -2,20 +2,49 @@
 
 #include "SortedMultiMap.h"
 
-SMMIterator::SMMIterator(const SortedMultiMap& d) : map(d) {
-  // TODO - Implementation
-}
+SMMIterator::SMMIterator(const SortedMultiMap& d) : map(d) { first(); }
 
-void SMMIterator::first() {}
+void SMMIterator::first() {
+  while (!stack.empty()) {
+    stack.pop();
+  }
+
+  current = map.root;
+  while (current != nullptr) {
+    stack.push(current);
+    current = current->left;
+  }
+
+  if (!stack.empty()) {
+    current = stack.peek();
+  } else {
+    current = nullptr;
+  }
+}
 
 void SMMIterator::next() {
-  // TODO - Implementation
+  if (!valid()) {
+    throw std::exception();
+  }
+
+  Node* node = stack.pop();
+
+  if (node->right != nullptr) {
+    node = node->right;
+    while (node != nullptr) {
+      stack.push(node);
+      node = node->left;
+    }
+  }
+
+  if (stack.empty()) {
+    current = nullptr;
+  } else {
+    current = stack.peek();
+  }
 }
 
-bool SMMIterator::valid() const {
-  // TODO - Implementation
-  return false;
-}
+bool SMMIterator::valid() const { return current != nullptr; }
 
 TElem SMMIterator::getCurrent() const {
   // TODO - Implementation
--- Semester_02/DSA/L5_SORTED_MULTI_MAP/SMMIterator.h
+++ Semester_02/DSA/L5_SORTED_MULTI_MAP/SMMIterator.h
@@ -4,6 +4,49 @@
 
 #include "SortedMultiMap.h"
 
+class NodeStack {
+ private:
+  Node** data;
+  int top, capacity;
+
+  void resize() {
+    capacity *= 2;
+    Node** newData = new Node*[capacity];
+    for (int i = 0; i < top; ++i) {
+      newData[i] = data[i];
+    }
+    delete[] data;
+    data = newData;
+  }
+
+ public:
+  NodeStack() : top(-1), capacity(10) { data = new Node*[capacity]; }
+  ~NodeStack() { delete[] data; }
+
+  void push(Node* node) {
+    if (top == capacity - 1) {
+      resize();
+    }
+    data[++top] = node;
+  }
+
+  Node* pop() {
+    if (top == -1) {
+      return nullptr;
+    }
+    return data[top--];
+  }
+
+  Node* peek() {
+    if (top == -1) {
+      return nullptr;
+    }
+    return data[top];
+  }
+
+  bool empty() { return top == -1; }
+};
+
 class SMMIterator {
   friend class SortedMultiMap;
 
@@ -11,6 +54,9 @@ class SMMIterator {
   // DO NOT CHANGE THIS PART
   const SortedMultiMap& map;
   SMMIterator(const SortedMultiMap& map);
+
+  NodeStack stack;
+  Node* current;
 
   // TODO - Representation
 
