311cb2c2a9ccf939762cf9c9de395bc3cf223c9d
Cristian Cretu <crisemcr@gmail.com>
2024-06-22 20:23:07 +0300

    a

--- Semester_02/OOP/exam/practice/microbial_world/window.cpp
+++ Semester_02/OOP/exam/practice/microbial_world/window.cpp
@@ -4,6 +4,8 @@ Window::Window(Session &session, Biologist &biologist, QWidget *parent)
     : session(session), biologist(biologist), QWidget(parent) {
   session.registerObserver(this);
   setWindowTitle(QString::fromStdString(biologist.get_name()));
+
+  QVBoxLayout *layout = new QVBoxLayout(this);
 }
 
 void Window::update() const {}
--- Semester_02/OOP/exam/practice/microbial_world/window.h
+++ Semester_02/OOP/exam/practice/microbial_world/window.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <QComboBox>
 #include <QLabel>
 #include <QLineEdit>
 #include <QMessageBox>
 #include <QPushButton>
+#include <QTableView>
 #include <QVBoxLayout>
 #include <QWidget>
 
@@ -14,6 +16,14 @@ class Window : public QWidget, public Observer {
  private:
   Session &session;
   Biologist &biologist;
+
+  QTableView *bacteriaTable;
+  QComboBox *speciesComboBox;
+
+  QLabel *nameLabel, *speciesLabel, *sizeLabel, *diseasesLabel;
+  QLineEdit *nameLineEdit, *speciesLineEdit, *sizeLineEdit, *diseasesLineEdit;
+
+  QPushButton *addButton, *removeButton, *updateButton;
 
  public:
   Window(Session &session, Biologist &biologist, QWidget *parent = Q_NULLPTR);
