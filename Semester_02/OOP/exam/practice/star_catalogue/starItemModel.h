0560b10f7c9800f663b208bdcc4ed252771c39d5
Cristian Cretu <crisemcr@gmail.com>
2024-06-26 10:24:12 +0300

    a

--- Semester_02/OOP/exam/practice/star_catalogue/starItemModel.h
+++ Semester_02/OOP/exam/practice/star_catalogue/starItemModel.h
@@ -9,49 +9,94 @@ class starItemModel : public QAbstractTableModel {
   Repository &repo;
 
  public:
-  explicit starItemModel(Repository &repository) : repo(repository) {}
+  starItemModel(Repository &repo) : repo(repo) {}
+  ~starItemModel() override = default;
+
   int rowCount(const QModelIndex &parent = QModelIndex()) const override {
-    return this->repo.getStars().size();
+    return repo.getStars().size();
   }
   int columnCount(const QModelIndex &parent = QModelIndex()) const override {
     return 5;
   }
   QVariant data(const QModelIndex &index,
                 int role = Qt::DisplayRole) const override {
-    int row = index.row(), column = index.column();
-    Star star = this->repo.getStars()[row];
-    if (role == Qt::DisplayRole) switch (column) {
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
+      auto stars = repo.getStars();
+      auto star = stars[index.row()];
+
+      switch (index.column()) {
         case 0:
           return QString::fromStdString(star.getName());
         case 1:
           return QString::fromStdString(star.getConstellation());
         case 2:
-          return QString::fromStdString(std::to_string(star.getRa()));
+          return (star.getRa());
         case 3:
-          return QString::fromStdString(std::to_string(star.getDec()));
+          return (star.getDec());
         case 4:
-          return QString::fromStdString(std::to_string(star.getDiameter()));
+          return (star.getDiameter());
+        default:
+          break;
       }
+    }
+
     return QVariant();
   }
   QVariant headerData(int section, Qt::Orientation orientation,
-                      int role) const override {
-    if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
-      switch (section) {
-        case 0:
-          return "Name of Star";
-        case 1:
-          return "Constellation";
-        case 2:
-          return "RA";
-        case 3:
-          return "Dec";
-        case 4:
-          return "Diameter";
+                      int role = Qt::DisplayRole) const override {
+    if (role == Qt::DisplayRole) {
+      if (orientation == Qt::Horizontal) {
+        switch (section) {
+          case 0:
+            return "Name";
+          case 1:
+            return "Constellation";
+          case 2:
+            return "RA";
+          case 3:
+            return "DEC";
+          case 4:
+            return "Diameter";
+          default:
+            break;
+        }
       }
     }
     return QVariant();
   }
+
+  bool setData(const QModelIndex &index, const QVariant &value,
+               int role = Qt::EditRole) override {
+    if (role != Qt::EditRole) {
+      return false;
+    }
+    Star &star = repo.getStars()[index.row()];
+    switch (index.column()) {
+      case 0:
+        star.setName(value.toString().toStdString());
+        break;
+      case 1:
+        star.setConstellation(value.toString().toStdString());
+        break;
+      case 2:
+        star.setRa(value.toInt());
+        break;
+      case 3:
+        star.setDec(value.toInt());
+        break;
+      case 4:
+        star.setDiameter(value.toDouble());
+        break;
+      default:
+        break;
+    }
+    emit dataChanged(index, index);
+    return true;
+  }
+  Qt::ItemFlags flags(const QModelIndex &index) const override {
+    return Qt::ItemIsEditable | QAbstractTableModel::flags(index);
+  }
+
   void updateData() {
     beginResetModel();
     endResetModel();
