13cd016b016293134ebc2ab971a49d17f741856c
Cristian Cretu <crisemcr@gmail.com>
2024-10-26 17:38:02 +0300

    feat: add 12b.pl

---
+++ Semester_03/PLF/Labs/Lab_03/12b.pl
@@ -0,0 +1,29 @@
+findDivs(N, D, []) :-
+    D >= N.
+findDivs(N, D, [D|Res]) :-
+    D < N,
+    N mod D =:= 0,
+    D1 is D + 1,
+    findDivs(N, D1, Res).
+findDivs(N, D, Res) :-
+    D < N,
+    N mod D =\= 0,
+    D1 is D + 1,
+    findDivs(N, D1, Res).
+
+singleList([], []).
+singleList([H|T], Res) :-
+    findDivs(H, 2, Divs),
+    merge([H|Divs], TailRes, Res),
+    singleList(T, TailRes).
+
+processHeteroList([], []).
+processHeteroList([[H|T]|TailList], [Res|ResList]) :-
+    singleList([H|T], Res),
+    processHeteroList(TailList, ResList).
+processHeteroList([H|TailList], [H|ResList]) :-  
+    processHeteroList(TailList, ResList).
+
+merge([], L, L).
+merge([H|T], L, [H|R]) :-
+    merge(T, L, R).
