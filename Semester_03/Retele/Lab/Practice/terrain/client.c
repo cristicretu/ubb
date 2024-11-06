ee2eceff0ac65d8fb240debfa3d4440881496879
Cristian Cretu <crisemcr@gmail.com>
2024-11-06 08:19:42 +0200

    feat: skeleton code

---
+++ Semester_03/Retele/Lab/Practice/terrain/client.c
@@ -0,0 +1,70 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define port_udp 7777
+#define port_tcp 1234
+#define N 5
+#define host "127.0.0.1"
+
+int main() {
+  int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (-1 == udp_sock) {
+    perror("Error in udp sock");
+    exit(1);
+  }
+
+  int tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == tcp_sock) {
+    perror("error in tcp sock");
+    close(udp_sock);
+    exit(1);
+  }
+
+  struct sockaddr_in udp_addr, tcp_addr;
+  memset(&udp_addr, 0, sizeof(udp_addr));
+  memset(&tcp_addr, 0, sizeof(tcp_addr));
+
+  udp_addr.sin_port = htons(port_udp);
+  udp_addr.sin_family = AF_INET;
+  udp_addr.sin_addr.s_addr = inet_addr(host);
+
+    tcp_addr.sin_port = htons(port_tcp);
+  tcp_addr.sin_family = AF_INET;
+  tcp_addr.sin_addr.s_addr = INADDR_ANY;
+
+  char buff[N + 1];
+
+  printf("got here\n");
+
+  while (1) {
+    // send random char and index after 5 sec
+
+    printf("got inside the while\n");
+    // listen for the new configuration
+    socklen_t addr_len = sizeof(udp_addr);
+    size_t recv_len;
+    if (-1 == (recv_len = recvfrom(udp_sock, buff, sizeof(buff), 0,
+                                   (struct sockaddr*)&udp_addr, &addr_len))) {
+      perror("Error on receiving from UDP");
+      close(udp_sock);
+      close(tcp_sock);
+      exit(1);
+    }
+    buff[recv_len] = '\0';
+
+    printf("got past the recvfrom\n");
+    printf("Received from brodcast: %s\n", buff);
+    break;
+  }
+
+  close(udp_sock);
+  close(tcp_sock);
+  return 0;
+}
---
+++ Semester_03/Retele/Lab/Practice/terrain/server.py
@@ -0,0 +1,68 @@
+import socket
+import threading
+import time
+
+port_udp = 7777
+port_tcp = 1234
+host = 'localhost'
+N = 5
+
+arr = ["u"] * N
+
+def is_explored(arr):
+  return not 'u' in arr
+
+# --------------------------
+
+def setup_tcp():
+  tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+  tcp_sock.bind((host, port_tcp))
+  tcp_sock.listen(7)
+
+  print(f"TCP Running on {host}:{port_tcp}")
+
+  return tcp_sock
+
+def setup_udp():
+  udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+  udp_sock.bind((host, port_udp))
+  print(f"UDP Running on {host}:{port_udp}")
+  return udp_sock
+
+def brodcast_arr(udp_sock):
+  arr_str = ''.join(arr)
+  print("Brodcasting the arr...")
+  udp_sock.sendto(arr_str.encode('utf-8'), ('127.0.0.1', port_udp))
+
+def handle_clients(client_sock, addr):
+  print(f"New connection from {addr}")
+
+  client_sock.close()
+
+def accept_clients(tcp_sock):
+  while not is_explored(arr):
+    try:
+      client_sock, addr = tcp_sock.accept()
+      threading.Thread(target=handle_clients, args=(client_sock, addr)).start()
+    except:
+      break
+
+
+if __name__ == "__main__":
+  udp_sock = setup_udp()
+  tcp_sock = setup_tcp()
+
+  thrd = threading.Thread(target=accept_clients, args=(tcp_sock,))
+  thrd.start()
+
+  while not is_explored(arr):
+    brodcast_arr(udp_sock)
+    time.sleep(2)
+
+  udp_sock.close()
+  tcp_sock.close()
