eef93d4b265180f208d265f9f12a8f09974a3025
Cristian Cretu <crisemcr@gmail.com>
2024-11-15 11:46:19 +0200

    feat: make it work for subsets of all length

--- Semester_03/PLF/Labs/Lab_04/2.pl
+++ Semester_03/PLF/Labs/Lab_04/2.pl
@@ -1,16 +1,42 @@
+% flow(i, i, i)
+collinear([_]).
+collinear([_,_]).
+collinear([[A,B], [M,N], [X,Y] | Rest]) :-
+    0 =:= A * (N - Y) + M * (Y - B) + X * (B - N),
+    collinear([[M,N], [X,Y] | Rest]).
+
+% flow(i ,i)
+apartine(X, [X|_]).
+apartine(X, [_|T]) :- apartine(X, T).
+
+% flow(i, i)
 subset([], []).
-subset([H|T], [H|R]):-
-  subset(T, R).
-subset([_|T], R):-
-  subset(T, R).
+subset([H|T], [H|R]):- subset(T, R).
+subset([_|T], R):- subset(T, R).
 
-collinear([[A, B], [M, N], [X, Y]]):-
-    0 =:= A * (N - Y) + M * (Y - B) + X * (B - N).
+% flow (i, i)
+isSol([], []).
+isSol(L, K):- 
+    subset(L, K),
+    my_length(K, Len),
+    Len >= 3, 
+    collinear(K).
 
-process([], []).
-process(L, K):-
-	subset(L, K),
-	collinear(K).
+% flow (i, o)
+colPoints(L, Solutions) :-
+    collectSolutions(L, [], Solutions).
 
-allSolutions(L, R) :-
-    findall(RPartial, process(L, RPartial), R).
+collectSolutions(L, Acc, Solutions) :-
+    isSol(L, Solution),
+    not(apartine(Solution, Acc)),
+    !, 
+    collectSolutions(L, [Solution|Acc], Solutions).
+collectSolutions(_, Solutions, Solutions).
+
+% flow(i, o)
+my_length(List, Res) :-
+  my_length(List, 0, Res).
+my_length([], Acc, Acc).
+my_length([_|T], Cnt, Res) :-
+  Cnt1 is Cnt + 1,
+  my_length(T,Cnt1, Res).
