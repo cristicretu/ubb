43e940712ffe5c9a77e4dd70c53c32788e89c38f
Cristian Cretu <crisemcr@gmail.com>
2024-11-25 20:10:36 +0200

    feat: damn, lisp got hands

--- Semester_03/PLF/Labs/Lab_05/1.lisp
+++ Semester_03/PLF/Labs/Lab_05/1.lisp
@@ -1,17 +1,53 @@
 ;; a) Write a function to return the n-th element of a list, or NIL if such an element does not exist.
-;; b) Write a function to check whether an atom E is a member of a list which is not necessarily linear.
+;; b) Write a function to check whether an atom E is a membru of a list which is not necessarily linear.
 ;; c) Write a function to determine the list of all sublists of a given list, on any level.
 ;; A sublist is either the list itself, or any element that is a list, at any level. Example:
-;; (12(3 (45) (67)) 8(9 10)) => 5 sublists :
-;; ((12(3(45) (67))8(910)) (3(45) (67)) (45) (67) (910))
+;; (1 2(3 (45) (67)) 8(9 10)) => 5 sublists :
+;; ((1 2(3(4 5) (6 7)) 8 (9 10)) (3(4 5) (6 7)) (4 5) (6 7) (9 10))
 ;; d) Write a function to transform a linear list into a set.
 
-(defun nth-elem (lista n)
-  (if (= n 1)
-    (car lista)
-    (nth-elem (cdr lista) (- n 1))
-  )
-)
+; problem a
+(defun n_th_element (lista n)
+  (if lista
+      (if (= n 1)
+          (car lista)
+          (n_th_element (cdr lista) (- n 1)))
+    NIL))
 
-(print (nth-elem `(1 2 3 4) 0))
-(print (nth-elem `(1 2 3 4) 3))
+; problem b
+(defun membru (e lista)
+  (if (null lista) 
+      NIL
+      (if (equal e (car lista))
+          t
+          (if (listp (car lista))
+              (if (membru e (car lista))
+                  t
+                  (membru e (cdr lista)))
+              (membru e (cdr lista))))))
+
+; problem c
+(defun sublists (lista)
+  (if (listp lista)
+      (append (list lista) (sublists-list lista))
+      NIL))
+
+(defun sublists-list (lista)
+  (if (null lista)
+      NIL
+      (append (sublists (car lista))
+              (sublists-list (cdr lista)))))
+
+
+; problem d
+(defun convert_to_set (lista)
+  (if (null lista)
+    NIL
+    (if (member (car lista) (cdr lista))
+      (convert_to_set (cdr lista))
+      (cons (car lista) (convert_to_set (cdr lista))))))
+
+(print (n_th_element '(1 2 3 4 5) 43))
+(print (membru 2 '(1 2 3 4 5)))
+(print (sublists '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+(print (convert_to_set '(1 2 2 2 2 2 2 3 3 3 3 4 4 4 4 4 5 5 5 5 5  6 6 6 6)))
