45f5074da18b9c0528680741f0e62d4152c69896
Cristian Cretu <crisemcr@gmail.com>
2025-01-04 14:14:55 +0200

    feat: save

---
+++ Semester_03/DB/Labs/Practice/Shoes/4.sql
@@ -0,0 +1,10 @@
+create function ShoesTShops
+(
+    @T int
+) returns table
+as return
+    select S.id
+    from Shoe S
+    join ShoePresentationShops SPS on S.id = SPS.shoe_id
+    group by S.id
+    having count(distinct  SPS.shop_id) >= @T
