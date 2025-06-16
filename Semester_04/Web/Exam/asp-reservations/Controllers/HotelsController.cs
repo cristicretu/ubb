539f44e069f9a453ca0856542de7d63b9c44f1ac
Cristian Cretu <crisemcr@gmail.com>
2025-06-16 09:39:07 +0300

    reserve a hotel

--- Semester_04/Web/Exam/asp-reservations/Controllers/HotelsController.cs
+++ Semester_04/Web/Exam/asp-reservations/Controllers/HotelsController.cs
@@ -1,20 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Data;
+using ProjectManagement.Models;
 
 namespace ProjectManagement.Controllers
 {
     public class HotelsController: Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HotelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Index()
         {
+            var date = HttpContext.Session.GetString("date");
+            var city = HttpContext.Session.GetString("city");
+            var hotels = _context.Hotels.Where(p => p.Date == date && p.City == city && p.AvailableRooms > 0).ToList();
+
+            ViewBag.Hotels = hotels;
+            ViewBag.Date = date;
+            ViewBag.City = city;
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(string name, string date, string city)
+        public IActionResult Reserve(int hotelId)
         {
-            
-            return View();
+            var hotel = _context.Hotels.Find(hotelId);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = new Reservations
+            {
+                IdReservedResource = hotelId,
+                Type = "hotel",
+                Person = HttpContext.Session.GetString("name")
+            };
+            _context.Reservations.Add(reservation);
+
+            hotel.AvailableRooms--;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 } 
